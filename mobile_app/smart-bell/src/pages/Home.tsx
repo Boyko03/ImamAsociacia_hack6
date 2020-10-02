a3df50f062ee842c165059199e68724d587a275b
Boyko03 <boyko100@abv.bg>
2020-10-02 16:38:37 +0300

    Now changing alarm button color

--- mobile_app/smart-bell/src/pages/Home.tsx
+++ mobile_app/smart-bell/src/pages/Home.tsx
@@ -6,6 +6,7 @@ import './Home.css';
 const Home: React.FC = () => {
   const [startAlarm, setStartAlarm] = useState<boolean>(false);
   const [alarmText, setAlarmText] = useState<string>("Start alarm");
+  const [alarmColor, setAlarmColor] = useState<string>("danger");
 
   return (
     <IonPage>
@@ -22,17 +23,19 @@ const Home: React.FC = () => {
         </IonHeader>
         <IonCard>
           <IonCardHeader>
-            <IonImg src="http://aif.guide.bg/images/bg.jpg"></IonImg>
+            <IonImg src="https://static-cdn.jtvnw.net/jtv_user_pictures/asmongold-profile_image-f7ddcbd0332f5d28-300x300.png"></IonImg>
           </IonCardHeader>
           <IonCardContent>
             <IonButton expand="block">Send photo again</IonButton>
-            <IonButton expand="block" color="danger" onClick={() => {
+            <IonButton expand="block" color={alarmColor} onClick={() => {
               setStartAlarm(!startAlarm);
               if(startAlarm){
                 setAlarmText("Start Alarm");
+                setAlarmColor("danger");
               }
               else{
                 setAlarmText("Stop Alarm");
+                setAlarmColor("medium");
               }
               }}>
               {alarmText}
