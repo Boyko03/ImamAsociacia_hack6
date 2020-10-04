5746793dc3962840308ef591a6bc8f8a27d94788
VikoTodorov <vikotodorov02@gmail.com>
2020-10-04 18:43:58 +0300

    refactoring and script for FCM

--- doorBellServer/server/face_recognition.py
+++ doorBellServer/server/face_recognition.py
@@ -5,9 +5,7 @@ import matplotlib.pyplot as plt
 
 
 def generateImage():
-    os.system("./runcam.sh")
-
-    image_raw = cv2.imread('./results/image.png', 0)
+    image_raw = cv2.imread('./results/frame.png', 0)
 
     haar_cascade_face = cv2.CascadeClassifier("/home/pi/opencv/data/haarcascades/haarcascade_frontalface_alt2.xml")
 
@@ -23,13 +21,12 @@ def detect_faces(scaleFactor = 1.1):
 
     faces_rect = image_result[1].detectMultiScale(image_result[0], scaleFactor=scaleFactor, minNeighbors=5)
     
-    print(faces_rect)
-
     for (x, y, w, h) in faces_rect:
         cv2.rectangle(image_copy, (x, y), (x+w, y+h), (0, 255, 0), 15)
 
-    return image_copy
-
+    return faces_rect
 
 if __name__ == "__main__":
-    detect_faces()
+    print(detect_faces())
+    #if(detect_faces() == ()):
+    #    print("AAAAAAAAAAAAAAAA")
